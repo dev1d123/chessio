39529d53eed9e707202e05c8565c22f24c5637bb
dev1d123 <dhuamanio@unsa.edu.pe>
2024-08-12 22:15:11 -0500

    Iniciando proyecto en Maven

---
+++ chessio/src/main/java/unsa/chessio/Chessio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package unsa.chessio;
+
+/**
+ *
+ * @author Windows
+ */
+public class Chessio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
