727d7832d9a6618e93c2f808a424c7fa8a9dcf83
Eduardo <emelendezr@unsa.edu.pe>
2024-08-19 23:57:58 -0500

    Logic for "Caballo" class has been implemented

--- chessio/src/main/java/source/Caballo.java
+++ chessio/src/main/java/source/Caballo.java
@@ -19,8 +19,63 @@ public class Caballo extends Pieza implements PiezaInterfaz{
 
     @Override
     public ArrayList<Pair> getMovimientos() {
-        ArrayList<Pair> res = new ArrayList<>();
-        
+        ArrayList<Pair> res = new ArrayList<Pair>();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                int distancia = distanciaLineal(i, j);
+                if (i == this.getX()) {
+                    if (distancia == 2) {
+                        if(posValida( i - 1, j)) { //Pos 1
+                            Pair p = new Pair(i - 1, j);
+                            res.add(p);
+                        } 
+                        if (posValida(i + 1, j)) {//Pos 2
+                            Pair p = new Pair(i + 1, j);
+                            res.add(p);
+                        }
+                    }
+                }
+                if (j == this.getY()) {
+                    if (distancia == 2) {
+                        if (posValida(i, j - 1)) { //Pos 1
+                            Pair p = new Pair(i, j - 1);
+                            res.add(p);
+                        }
+                        if (posValida(i, j + 1)) { //Pos 2
+                            Pair p = new Pair(i, j + 1);
+                            res.add(p);
+                        }
+                    }
+                
+                }
+            }
+        }
         return res;
+
+    }
+    
+    private int distanciaLineal(int fila, int columna) {
+        int rpta = 0;
+        rpta += Math.abs(this.getX() - fila);
+        rpta += Math.abs(this.getY() - columna);
+        return rpta;
+    }
+    
+    private boolean posValida(int fila, int columna) {
+        return (fila >= 0 && columna >= 0 && fila <= 7 && columna <= 7);
+    }
+    
+    public String getM(){
+        ArrayList<Pair> res = getMovimientos();
+        String ans = "";
+        for(Pair p: res){
+            ans+= "("+p.X+", "+p.Y+")\n";
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        Caballo t = new Caballo(4, 4);
+        System.out.println(t.getM());
     }
 }
+
