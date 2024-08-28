ecc8e63031664f78fada6c28dd7ebaf57ed8cfde
Julio Chura <jchuraaca@unsa.edu.pe>
2024-08-27 23:53:34 -0500

    Se agregan las ventanas pero surge un error al correr el tablero (se congela la aplicacion)

--- chessio/src/main/java/game/Game.java
+++ chessio/src/main/java/game/Game.java
@@ -6,13 +6,16 @@ import source.*;
 
 public class Game {
     public static void main(String args[]){
-        System.out.println("Iniciando el juego, creando el juego!");            
-        Juego j = new Juego();
+        System.out.println("Iniciando el juego, creando el juego!");    
+        
+        Menu menu = new Menu();
+        menu.setVisible(true);
+        //Juego j = new Juego();
 
         System.out.println("Dandole la informacion al tablero");
-        TableroGUI tablero = new TableroGUI(j);
+        //TableroGUI tablero = new TableroGUI(j);
         JOptionPane.showMessageDialog(null, "El tablero ha sido creado");
-        j.iniciarJuego(tablero);
+        //j.iniciarJuego(tablero);
         
     }  
 }
